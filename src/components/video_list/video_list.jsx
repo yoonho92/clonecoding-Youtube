82b6f33b88385eea8bee2d6e86ad19b29e9aa629
yoonho <yoonho2015@gmail.com>
2022-02-22 14:22:25 +0900

    비디오 리스트를 나열하는 컴포넌트를 생성하다.

---
+++ src/components/video_list/video_list.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import VideoItem from "../video_item/video_item";
+
+const VideoList = (props) => {
+  return (
+    <ul>
+      {props.videos &&
+        props.videos.map((video) => <VideoItem key={video.id} video={video} />)}
+    </ul>
+  );
+};
+export default VideoList;
