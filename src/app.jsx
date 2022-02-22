76fea029e02f05f800feb3ce68dd56324ef9dc29
yoonho <yoonho2015@gmail.com>
2022-02-22 14:23:21 +0900

    youtube api를 통해 popular video 리스트를 호출하다.

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,26 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-    return <h1>Hello :)</h1>
+  const [videos, setVideos] = useState();
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDKukKoXKk225xxQUUFS-ufTgt_Yy6STvw",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
