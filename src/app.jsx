27eda8e6670aad745cff0ec0e3ce81a35e34e842
yoonho <yoonho2015@gmail.com>
2022-02-24 00:15:30 +0900

    개별 비디오 컴포넌트의 디테일을 만들다.

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,20 @@
 import { useEffect, useState } from "react";
 import styles from "./app.module.css";
 import SearchHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 import VideoList from "./components/video_list/video_list";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState();
+  const [selectedVideo, setSelectedVideo] = useState();
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
+
   const search = (query) => {
+    setSelectedVideo(null);
+
     youtube
       .search(query) //
       .then((items) => setVideos(items));
@@ -20,7 +29,20 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            {<VideoDetail video={selectedVideo} />}
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
     </div>
   );
 }
--- src/service/youtube.js
+++ src/service/youtube.js
@@ -7,22 +7,21 @@ class Youtube {
     };
   }
 
-  mostPopular() {
-    return fetch(
+  async mostPopular() {
+    const response = await fetch(
       `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
       this.getRequestOptions
-    ).then((response) => response.json());
+    );
+    return await response.json();
   }
 
-  search(query) {
-    return fetch(
+  async search(query) {
+    const response = await fetch(
       `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
       this.getRequestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      );
+    );
+    const result_1 = await response.json();
+    return result_1.items.map((item) => ({ ...item, id: item.id.videoId }));
   }
 }
 
